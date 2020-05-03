e0aba827eee971170f4ab205177a8fbd8d3f291a
Miguel Ruiz <migz.p.ruiz@gmail.com>
2020-05-03 15:34:39 -0700

    Adding the run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="migzruiz/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run api\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward api 8000:80
